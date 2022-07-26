19fd5b0119dba04543fe762f4ed5a6631c87ae53
Petr <petr.galunov@yandex.ru>
2022-07-26 17:31:09 +0300

    Task #1

---
+++ src/leetcode/twoSum.go
@@ -0,0 +1,34 @@
+package leetcode
+
+// v1
+func TwoSumv1(nums []int, target int) []int {
+	ans := []int{0, 0}
+	nset := map[int]int{}
+	for n := range nums {
+		if _, ok := nset[nums[n]]; !ok {
+			nset[nums[n]] = n
+		}
+	}
+	for n := range nums {
+		if index, ok := nset[target-nums[n]]; ok && n != index {
+			ans[0] = n
+			ans[1] = index
+		}
+	}
+	return ans
+}
+
+// v2
+func TwoSumv2(nums []int, target int) []int {
+	var ans []int
+	nset := map[int]int{}
+	for n := range nums {
+		if _, ok := nset[nums[n]]; !ok {
+			nset[nums[n]] = n
+		}
+		if index, ok := nset[target-nums[n]]; ok && n != index {
+			ans = []int{n, index}
+		}
+	}
+	return ans
+}
